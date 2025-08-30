0b9894c2330d8c642665b3416564e9232928c2c9
sd <sdfox05@gmail.com>
2025-08-30 11:16:32 +0800

    lab3: print a page table
    	modified:   kernel/defs.h
    	modified:   kernel/exec.c
    	modified:   kernel/vm.c

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,33 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+printwalk(pagetable_t pagetable, uint64 level)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+
+      for(int j = 1;j < level;j++)
+        printf(".. ");
+      printf("..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      printwalk((pagetable_t)child, level + 1);
+    } else if(pte & PTE_V){
+      // leaf
+      for(int j = 1;j < level;j++)
+        printf(".. ");
+      printf("..%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  printwalk(pagetable, 1);
+}
