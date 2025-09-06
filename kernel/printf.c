3de4801a0a64ede378c68d748280491d8255a1cd
sd <sdfox05@gmail.com>
2025-09-06 10:53:38 +0800

    lab4: backtrace
    modified:   kernel/defs.h
    modified:   kernel/printf.c
    modified:   kernel/riscv.h
    modified:   kernel/sysproc.c

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,16 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+void
+backtrace(void)
+{
+  uint64 fp = r_fp(), ra;
+
+  printf("backtrace:\n");
+  while(fp > PGROUNDDOWN(fp) && fp < PGROUNDUP(fp)){
+    asm volatile("ld %0, -8(%1)" : "=r" (ra) : "r" (fp));
+    printf("%p\n", ra);
+    asm volatile("ld %0, -16(%1)" : "=r" (fp) : "r" (fp));
+  }
+}
